09874f47551fa871a14eb587cfb5a2aa47e69832
wellington.aferreir2 <wellington.aferreir2@TIT0545461W10-1>
2025-02-25 19:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Bem vindo!");
+Console.WriteLine("Wellington");
+Console.WriteLine("Boa noite");
+Console.Clear();
+
+string firstName = "Wellington";
+string lastName = "Ferreira";
+string surname = "Araujo";
+
+
+Console.WriteLine("Digite sua idade: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Nome Completo: {firstName} {surname} {lastName} \nIdade é: {age}");
+
+Console.ReadKey();
