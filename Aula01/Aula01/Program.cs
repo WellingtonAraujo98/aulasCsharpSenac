d37a3c10f5c93d584d9b3dec24aa45719cd9d3d0
wellingtonAraujo98 <wellington.a14@hotmail.com>
2025-02-25 19:40:45 -0300

    Foi adicionado um comentario de teste.

--- Aula01/Aula01/Program.cs
+++ Aula01/Aula01/Program.cs
@@ -10,7 +10,7 @@ string surname = "Araujo";
 
 Console.WriteLine("Digite sua idade: ");
 int age = int.Parse(Console.ReadLine());
-
+//teste git
 Console.WriteLine($"Nome Completo: {firstName} {surname} {lastName} \nIdade é: {age}");
 
 Console.ReadKey();
