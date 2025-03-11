2621009cff5a6243582ab6d5ad53a6908f530e55
WELLINGTON ARAUJO FERREIRA <Wellington.a14@hotmail.com>
2025-03-10 22:11:24 -0300

    Começo cadastro de alunos

---
+++ SistemaDeCadastroAlunos/SistemaDeCadastroAlunos/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeCadastroAlunos
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public string Curso;
+        public int Matricula;
+
+        public Aluno()
+        {
+        }
+
+        public string AdicionarNome() 
+        {
+            Console.WriteLine("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+            return nome;
+        }
+        public void AdicionarCurso()
+        {
+            Console.WriteLine("Qual o curso do aluno? ");
+            Curso = Console.ReadLine();
+        }
+        public void AdicionarMatricula()
+        {
+            Console.WriteLine("Qual a matricula do aluno? ");
+            Matricula = int.Parse(Console.ReadLine());
+        }
+
+
+
+    }
+}
---
+++ SistemaDeCadastroAlunos/SistemaDeCadastroAlunos/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SistemaDeCadastroAlunos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> listaAlunos = new List<Aluno>();
+            bool menu = true;
+            while (menu) 
+            {
+
+                int opcao;
+                Console.WriteLine("Digite 1 para adicionar um aluno: ");
+                Console.WriteLine("Digite 2 para exibir a lista de alunos: ");
+                Console.WriteLine("Digite 0 para fechar o menu: ");
+                opcao = int.Parse(Console.ReadLine());
+                if (opcao == 0)
+                {
+                    menu = false;  
+                    break;
+                }
+                if (opcao == 1) 
+                {
+                    Console.WriteLine("Adicionando aluno");
+                }
+                if (opcao == 2) 
+                {
+                    Console.WriteLine("Menu");
+                    foreach (Aluno aluno in listaAlunos)
+                    {
+                        Console.WriteLine($"Nome: {aluno.Nome}");
+                        Console.WriteLine($"E-mail: {aluno.Curso}");
+                        Console.WriteLine($"Matricula: {aluno.Matricula}");
+                        Console.WriteLine("");
+                    }
+                }
+                
+                
+
+                foreach (Aluno aluno in listaAlunos)
+                {
+                    Console.WriteLine($"Nome: {aluno.Nome}");
+                    Console.WriteLine($"E-mail: {aluno.Curso}");
+                    Console.WriteLine($"Matricula: {aluno.Matricula}");
+                    Console.WriteLine("");
+                }
+
+            }
+
+            
+            
+
+            
+        }
+    }
+}
