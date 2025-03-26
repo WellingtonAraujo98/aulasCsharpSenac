c22f6fa0c95be5047bfe09df66233340a754303f
WELLINGTON ARAUJO FERREIRA <93162399+WellingtonAraujo98@users.noreply.github.com>
2025-03-25 22:16:21 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace testePoo;
+
+public class ContaBancaria
+{
+	public string NumeroConta;
+	public string Nome;
+	private double _saldo;
+	
+
+	ContaBancaria(string conta, string nome)
+	{
+		NumeroConta = conta;
+		Nome = nome;
+		_saldo = 0;
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace testePoo;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("teste" );
+
+        ContaBancaria minhaConta = new ContaBancaria("545454", "Well");
+        minhaConta.Nome;
+    }
+}
